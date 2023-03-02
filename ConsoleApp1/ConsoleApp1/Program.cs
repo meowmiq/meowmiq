ff89d668e8b430ee13be93283abd11150b3a639f
meowmiq <126751707+meowmiq@users.noreply.github.com>
2023-03-02 15:10:15 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+    static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+    static double Sum(double a, double b)
+    {
+        return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+        double result = Sum(7.9, 5.3);
+        Console.WriteLine(result);
+    }
+}
